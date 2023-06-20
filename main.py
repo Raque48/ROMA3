784c109bbac33b3ac5040f467998029462c81caf
jona4815 <99785626+Raque48@users.noreply.github.com>
2023-06-20 17:39:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -448,4 +448,4 @@ def display_selected_data(selectedData, chart_dropdown, year):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=False, host='0.0.0.0:5000')
